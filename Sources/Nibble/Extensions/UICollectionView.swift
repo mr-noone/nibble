dacbe2ceeaad563fccef70e29423255caea6ffd3
Aleksey Zgurskiy <mr.noone@icloud.com>
2021-03-25 11:43:32 +0200

    Add collection views extension

---
+++ Sources/Nibble/Extensions/UICollectionView.swift
@@ -0,0 +1,23 @@
+//
+//  UICollectionView.swift
+//  Nibble
+//
+//  Created by Aleksey Zgurskiy on 24.03.2021.
+//  Copyright © 2020 Aleksey Zgurskiy. All rights reserved.
+//
+
+import UIKit
+
+public extension UICollectionView {
+  func dequeue<T: UICollectionViewCell & Reusable>(with identifire: String = T.reuseIdentifier, for indexPath: IndexPath) -> T {
+    return dequeueReusableCell(withReuseIdentifier: identifire, for: indexPath) as! T
+  }
+  
+  func register<T: UICollectionViewCell & Reusable>(_ aClass: T.Type, for identifier: String = T.reuseIdentifier) {
+    if let aClass = aClass as? NibRepresentable.Type {
+      register(aClass.nib, forCellWithReuseIdentifier: identifier)
+    } else {
+      register(aClass, forCellWithReuseIdentifier: identifier)
+    }
+  }
+}
